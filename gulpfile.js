6e6f49920f3a0052de71fe3a29d4654a6a8ab390
cristobal <cristobal.badillo@gmail.com>
2017-09-18 07:48:56 -0500

    esttructura de directorios

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+const gulp= require("gulp");
+const browserSync= require("browser-sync").create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('serve', () => {
+    browserSync.init({
+        server: "./app/dist/slider/"
+    });
+
+    gulp.watch("./app/dist/slider/*.html",['html']);
+    gulp.watch("./app/dist/slider/*.js",['html']);
+    gulp.watch("./app/dist/slider/*.css",['css']);
 });
+
+gulp.task('css',() => {
+  gulp.src("./app/dist/slider*.css")
+  .pipe(browserSync.reload())
+});
+
+gulp.task('html',() => {
+  browserSync.reload();
+});
+
+
+
+gulp.task('default', ['serve']);
